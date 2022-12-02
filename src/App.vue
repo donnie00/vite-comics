7dd7f84a66fd083c2155efb49a40361dc35eeb13
ippoRulez <ippolitodonato@gmail.com>
2022-12-02 16:08:05 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import TheHeader from './components/TheHeader.vue';
+export default {
+	components: {TheHeader},
+};
+</script>
+
+<template>
+	<h1>Hello word</h1>
+	<TheHeader></TheHeader>
+</template>
+
+<style></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+	data() {
+		return {};
+	},
+};
+</script>
+
+<template>
+	<h1>Component?</h1>
+</template>
